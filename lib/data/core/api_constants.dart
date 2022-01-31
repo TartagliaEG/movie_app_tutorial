e492b373a0317f7351ccb9b3524fcd549c2484ea
TartagliaEG <erikt@ciandt.com>
2022-01-31 14:08:05 -0300

    ajustes para funcionamento do app

--- lib/data/core/api_constants.dart
+++ lib/data/core/api_constants.dart
@@ -2,6 +2,6 @@ class ApiConstants {
   ApiConstants._();
 
   static const String BASE_URL = "https://api.themoviedb.org/3/";
-  static const String API_KEY = "6247fdf2489366ec54d937992c00e5d1";
+  static const String API_KEY = "f33521953035af3fc3162fe1ac22e60c";
   static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 }
